2fd38087fe6841c16080a0bee753fcca2e7aa542
tergog <tergog@mail.ru>
2020-04-28 12:00:21 +0300

    Page 9 mobile + Desktop completed

--- src/app/shared/components/slide-toggle/slide-toggle.component.ts
+++ src/app/shared/components/slide-toggle/slide-toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slide-toggle',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slide-toggle.component.scss']
 })
 export class SlideToggleComponent implements OnInit {
+
+  @Input() selected: boolean;
 
   constructor() { }
 
